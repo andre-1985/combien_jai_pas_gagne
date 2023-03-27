b48c2c5d4bec2af1acf5f310997524fb13d4364e
andre-1985 <andre.appaoo@gmail.com>
2023-03-27 07:17:16 +0200

    Add webapp packages

---
+++ src/Controller/DrawsEuromillionsController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\DrawEuromillionsRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DrawsEuromillionsController extends AbstractController
+{
+    #[Route('/tiragesEuromillions', name: 'draws_euromillions')]
+    public function index(
+        DrawEuromillionsRepository $repository,
+        PaginatorInterface $paginator,
+        Request $request
+    ): Response
+    {
+        $drawsEuromillions = $repository->findAll();
+
+        $allDrawsEuromillions = $paginator->paginate(
+            $drawsEuromillions,
+            $request->query->getInt('page', 1),
+            20
+        );
+
+        return $this->render('pages/draws_euromillions/index.html.twig', [
+            'allDrawsEuromillions' => $allDrawsEuromillions,
+        ]);
+    }
+}
